1c01534ffcf3b4047dc2b7c025def04d9381abc5
ThalyssonHenrique <thalyshenriique@gmail.com>
2024-01-07 10:49:53 -0300

    conclusão do projeto

--- scripts.js
+++ scripts.js
@@ -1,39 +1,67 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
+const buttonSumAll = document.querySelector('.sum-all')
+const buttonFilterAll = document.querySelector('.filter-all')
 
-buttonShowAll.addEventListener('click', ()=> clickShowAll(menuOptions))
+
+buttonFilterAll.addEventListener('click', clickFilterAll)
+buttonSumAll.addEventListener('click', clickSumAll)
+buttonShowAll.addEventListener('click', () => clickShowAll(menuOptions))
 buttonMapAll.addEventListener('click', clickMapAll)
+
+
+function currencyFormat(value){
+
+    const formatValue = value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+    return formatValue
+}
+
 
 function clickShowAll(productsArray) {
 
-   let myList = ''
+    let myList = ''
 
-    productsArray.forEach((list) => {
+    productsArray.forEach(list => {
 
         myList += `
         <li>
         <img src=${list.src}>
         <p class="option-burguer">${list.name}</p>
-        <p class="burguer-price">R$ ${list.price.toFixed(2)}</p>
-        </li>
-        `
+        <p class="burguer-price"> ${currencyFormat(list.price)}</p>
+    </li>
+    `
     });
 
     list.innerHTML = myList
-
 }
 
 function clickMapAll() {
 
-    const newValue = menuOptions.map((list) => ({
+    const newPrices = menuOptions.map(list => ({
         ...list,
         price: list.price * 0.9
-
     }))
 
-    clickShowAll(newValue)
+    clickShowAll(newPrices)
 }
 
+function clickSumAll(){
 
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+    list.innerHTML = `
+    <li>
+    <p class="option-burguer">O valor total dos produtos é <br> ${currencyFormat(totalValue)}</p>
+    </li>
+`
+}
+
+function clickFilterAll(){
+
+    const filterJustVegan = menuOptions.filter((productsVegan) => productsVegan.vegan)
+
+    clickShowAll(filterJustVegan)
+}
 
